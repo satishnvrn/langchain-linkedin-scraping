84ba824a678b91f9f7cd73f8a9919c73ab4dcc6f
satishnvrn <nvrn.satish@gmail.com>
2024-09-11 23:07:44 +0530

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# This project builds a simple and basic application that scrapes linkeding profile information
+
+## Install dependencies
+
+> [Install Anaconda](https://docs.conda.io/projects/conda/en/4.6.0/user-guide/install/index.html#installation).
+>
+> Create a similar environment using conda: `conda create -p llsenv1 python=3.10 -y`
+>
+> Activate venv environment: `conda activate ./llsenv1`
+>
+> Deactivate venv environment: `conda deactivate`
+>
+> Install python packages from requirements.txt: `pip install -r requirements.txt`
---
+++ ice_breaker.py
@@ -0,0 +1,30 @@
+from langchain.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+from third_parties.linkedin import scrape_linkedin_profile
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    summary_template = """
+    given the Linkedin information {information} about a person from I want you to create:
+    1. a short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+
+    chain = summary_prompt_template | llm
+    linkedin_data = scrape_linkedin_profile(
+        "https://www.linkedin.com/in/eden-marco-866555128/", mock=True
+    )
+    res = chain.invoke({"information": linkedin_data})
+
+    print(res)
